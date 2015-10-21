12d9f3b4fb95a3386ef4028b541ec9f086094ba3
Rinzler838 <dvil4688@740s-pl-105.local>
2015-10-21 08:29:27 -0600

    Created Chatbot Application
    Allows interaction between computer and user though Strings.

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,16 @@
+package chat.controller;
+
+/**
+ * Starts the program.
+ * @author Deigen Villalobos
+ * @version 1.0 10/21/15
+ */
+
+public class ChatRunner
+{
+	public static void main (String [] args)
+	{
+		//ChatController myController = new ChatController();
+		//myController.start();
+	}
+}
