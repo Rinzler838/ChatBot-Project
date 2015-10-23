e5a42a829fb6da488ae640c8282a50cc62d9abd7
Rinzler838 <dvil4688@740s-pl-105.local>
2015-10-23 08:20:24 -0600

    Added String Length Checker

--- src/chat/model/ChatBot.java
+++ src/chat/model/ChatBot.java
@@ -42,7 +42,23 @@ public class ChatBot
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
-		return false;
+		boolean hasLength = false;
+		
+		if (currentInput != null)
+		{
+			if (currentInput.length() != 1)
+			{
+				hasLength = true; 
+			}
+		}
+		
+		//Alternate method: Compound "if"
+		//if (currentInput != null && currentInput.length() >0)
+		//{
+		//	hasLength = true;
+		//}
+		
+		return hasLength;
 	}
 	
 	/**
