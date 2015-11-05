bc0675bf24c498832e03fc7e4b7f5ddb26a75099
Rinzler838 <dvil4688@740s-pl-105.local>
2015-11-05 08:52:38 -0700

    Added documentation

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
 /**
- * Popup display class for GUI interaction in the Chatbot Project
+ * Popup display class for GUI interaction in the Chatbot Project.
  * @author dvil4688
- * @version 1.3 11/5/15 Added icons to popup window
+ * @version 1.3 11/5/15 Added icons to popup window.
  */
 
 public class ChatView
@@ -19,11 +19,13 @@ public class ChatView
 		windowMessage = "Greetings, it's your friendly neighborhood chat bot...";
 		chatIcon = new ImageIcon(getClass().getResource("images/robot.png"));
 	}
-
-	public void showMessage(String myDisplay)
-	{
-		JOptionPane.showMessageDialog(null, myDisplay, windowMessage, JOptionPane.INFORMATION_MESSAGE, chatIcon);
-	}
+	
+	/**
+	 * Displays a GUI popup for collecting user text with the supplied String.
+	 * Usually in the form of a question to collect for further use in the program.
+	 * @param myDisplay The text to show in the popup window. Should be a question.
+	 *  @return The text supplied by the user.
+	 */
 	
 	public String chatInput(String displayText)
 	{
@@ -32,6 +34,16 @@ public class ChatView
 		userInput = JOptionPane.showInputDialog(null, displayText, windowMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Enter text here...").toString();
 		
 		return userInput;
+	}
+	
+	/**
+	 * Displays a String to the user with a standard popup.
+	 * @param myDisplay The supplied text to be displayed.
+	 */
+	
+	public void showMessage(String myDisplay)
+	{
+		JOptionPane.showMessageDialog(null, myDisplay, windowMessage, JOptionPane.INFORMATION_MESSAGE, chatIcon);
 	}
 
 }
