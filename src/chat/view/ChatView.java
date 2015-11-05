029b38128d32bc9e85d55dcdc24124f48f9701ac
Rinzler838 <dvil4688@740s-pl-105.local>
2015-11-05 08:51:37 -0700

    Added improved GUI with icons

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,22 +1,37 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+/**
+ * Popup display class for GUI interaction in the Chatbot Project
+ * @author dvil4688
+ * @version 1.3 11/5/15 Added icons to popup window
+ */
 
 public class ChatView
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
 	
+	public ChatView()
+	{
+		windowMessage = "Greetings, it's your friendly neighborhood chat bot...";
+		chatIcon = new ImageIcon(getClass().getResource("images/robot.png"));
+	}
+
 	public void showMessage(String myDisplay)
 	{
-		JOptionPane.showMessageDialog(null, myDisplay);
+		JOptionPane.showMessageDialog(null, myDisplay, windowMessage, JOptionPane.INFORMATION_MESSAGE, chatIcon);
 	}
 	
 	public String chatInput(String displayText)
 	{
-		String returnedText = "";
+		String userInput = "";
 		
-		returnedText = JOptionPane.showInputDialog(null, displayText);
+		userInput = JOptionPane.showInputDialog(null, displayText, windowMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Enter text here...").toString();
 		
-		return returnedText;
+		return userInput;
 	}
 
 }
