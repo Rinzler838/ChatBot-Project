7fe119e02e16d7499b66235f78865cd946992e3d
Rinzler838 <dvil4688@740s-pl-105.local>
2016-03-07 09:04:34 -0700

    Constructed ArrayLists

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import twitter4j.*;
 
 /**
- * @author dvil4688
- * @version 0.1
+ * @author Deigen Villalobos
+ * @version 0.2 Reference to Twitter4j
  * This Class will allow ChatBot to access Twitter, interact with other users Tweets, and Tweet itself.
  */
 public class CTECTwitter
 {
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>(); 
+	}
 }
