3c5bba6304cad44da3edae7f73473f092dcfa087
Rinzler838 <dvil4688@740s-pl-105.local>
2016-03-07 08:51:50 -0700

    Created CTECTwitter class and added required files

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,13 @@
+package chat.model;
+
+import java.util.ArrayList;
+
+/**
+ * @author dvil4688
+ * @version 0.1
+ * This Class will allow ChatBot to access Twitter, interact with other users Tweets, and Tweet itself.
+ */
+public class CTECTwitter
+{
+	
+}
