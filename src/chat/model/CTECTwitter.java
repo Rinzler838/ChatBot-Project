876613678fdee88853463b254cb8732ea43f4954
Rinzler838 <dvil4688@740s-pl-105.local>
2016-03-07 09:14:59 -0700

    Added sendTweet method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,21 +1,36 @@
 package chat.model;
 
 import java.util.ArrayList;
+
 import twitter4j.*;
 
 /**
  * @author Deigen Villalobos
- * @version 0.2 Reference to Twitter4j
+ * @version 0.3 Constructed ArrayLists
  * This Class will allow ChatBot to access Twitter, interact with other users Tweets, and Tweet itself.
  */
 public class CTECTwitter
 {
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
 	
 	public CTECTwitter()
 	{
 		statusList = new ArrayList<Status>();
 		wordList = new ArrayList<String>(); 
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		try
+		{
+			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		}
+		catch (TwitterException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
